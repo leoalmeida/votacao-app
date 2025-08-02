222224d25fd06b333d140b531d76c6522a4eeea2
Leonardo Almeida <leoalmeida.rj@gmail.com>
2025-08-02 11:30:10 -0300

    Correções no layout da tela

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home-page/home.component';
+import { AdminComponent } from './admin-page/admin.component';
+import { SessionCardComponent } from './components/session-card/session-card.component';
+import { NotFound } from './components/not-found/not-found.component';
+import { PautaDetailsComponent } from './components/pauta-details/pauta-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    component: HomeComponent,
+    title: 'Home Page'
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    title: 'Admin'
+  },
+  {
+    path: 'pauta',
+    component: PautaDetailsComponent,
+    title: 'Nova pauta'
+  },
+  { path: '**', component: NotFound }
+];
